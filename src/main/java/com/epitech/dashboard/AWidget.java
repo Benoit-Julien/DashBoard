8f3c488022b53e25d848f7a2ae781f9b38df66fd
Malardrim <marc.pazat@epitech.eu>
2018-04-14 15:50:33 +0200

    Abstract widget first usable version

--- src/main/java/com/epitech/dashboard/AWidget.java
+++ src/main/java/com/epitech/dashboard/AWidget.java
@@ -4,6 +4,19 @@ import com.vaadin.ui.*;
 
 abstract public class AWidget {
 
+    protected class MainLayout extends VerticalLayout{
+        public static final String CLASSNAME = "MainLayout";
+
+        public MainLayout(){
+            setStyleName(CLASSNAME);
+            setWidth("200px");
+            setHeight("200px");
+        }
+    }
+
+    /**
+     * Name to be displayed in the combo box
+     */
     private String name;
 
     /**
@@ -19,7 +32,7 @@ abstract public class AWidget {
     /**
      * Main component, this is the component who must be displayed
      */
-    protected Component mainDisplay = null;
+    protected MainLayout mainDisplay = new MainLayout();
 
     /**
      * Layout to contain the form to instantiate a widget
@@ -31,25 +44,56 @@ abstract public class AWidget {
      */
     private Button submitButton = new Button("Submit");
 
+    /**
+     * Constructor mainly to use in case of instantiation of a model
+     * @param uid Unique Id of the model
+     * @param name Unique name of the model
+     */
     protected AWidget(int uid, String name){
         this.uid = uid;
         this.name = name;
         submitButton.addClickListener(e -> this.submitted());
-        formWindow.setContent(formContent);
+        VerticalLayout layout = new VerticalLayout();
+        layout.addComponent(formContent);
+        formWindow.setContent(layout);
+        layout.setComponentAlignment(formContent, Alignment.MIDDLE_CENTER);
         formWindow.center();
-        formWindow.setSizeFull();
     }
 
+    /**
+     * Adds a listener to the submit button
+     * @param listener Listener to be attached
+     */
+    public void addSubmitListener(Button.ClickListener listener){
+        submitButton.addClickListener(listener);
+    }
+
+    /**
+     * Copy constructor
+     * @param widget Widget to copy
+     */
+    public AWidget(AWidget widget){
+        uid = widget.uid;
+        name = widget.name;
+        mainDisplay = new MainLayout();
+        formContent = widget.formContent;
+    }
+
+    /**
+     * Unique id getter
+     * @return Unique id of the model
+     */
     public int getUid() {
         return uid;
     }
 
     /**
-     *
+     * Getter for the form window
      * @return Window to be displayed
      */
     public Window getFormWindow(){
         formContent.addComponent(submitButton);
+        formContent.setComponentAlignment(submitButton, Alignment.BOTTOM_LEFT);
         return formWindow;
     }
 
@@ -60,9 +104,15 @@ abstract public class AWidget {
     public abstract void refresh();
 
     /**
-     * Method will be called when the form is ok
+     * Clone the model into a viable widget
+     * @return A cloned widget
      */
-    protected abstract void submitted();
+    public abstract AWidget clone();
+
+    /**
+     * Method will be called when the form is submitted
+     */
+    protected abstract boolean submitted();
 
     public Component getComponent(){
         return mainDisplay;
