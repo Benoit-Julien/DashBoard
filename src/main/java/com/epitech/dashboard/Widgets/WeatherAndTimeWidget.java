22c0e5fae2b66d080aa3e2229301e5279f34302c
Julien <julien1.benoit@epitech.eu>
2018-05-04 14:45:19 +0200

    Test Open Weather Map Api

--- src/main/java/com/epitech/dashboard/Widgets/WeatherAndTimeWidget.java
+++ src/main/java/com/epitech/dashboard/Widgets/WeatherAndTimeWidget.java
@@ -1,12 +1,41 @@
 package com.epitech.dashboard.Widgets;
 
 import com.epitech.dashboard.Widget;
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
 
 public class WeatherAndTimeWidget extends AWidget {
 
-    public WeatherAndTimeWidget(int uid)
+    public WeatherAndTimeWidget()
     {
         super("Weather and Time");
+        OWM owm = new OWM("760df4191f118ae3e6155d29c549ab90");
+
+        // getting current weather data for the "London" city
+        CurrentWeather cwd = null;
+        try {
+            cwd = owm.currentWeatherByCityName("London");
+        } catch (APIException e) {
+            e.printStackTrace();
+        }
+
+        //printing city name from the retrieved data
+        System.out.println("City: " + cwd.getCityName());
+
+        // printing the max./min. temperature
+        System.out.println("Temperature: " + cwd.getMainData().getTempMax()
+                + "/" + cwd.getMainData().getTempMin() + "\'K");
+    }
+
+    public WeatherAndTimeWidget(AWidget source)
+    {
+        super(source);
+        if (source instanceof RSSFeedWidget) {
+
+        }
+        else
+            throw new IllegalArgumentException(CLONE_ERR);
     }
 
     @Override
@@ -16,7 +45,7 @@ public class WeatherAndTimeWidget extends AWidget {
 
     @Override
     public AWidget clone() {
-        return null;
+        return new WeatherAndTimeWidget(this);
     }
 
     @Override
@@ -26,11 +55,13 @@ public class WeatherAndTimeWidget extends AWidget {
 
     @Override
     public Widget SaveWidget() {
-        return null;
+        Widget save = new Widget();
+        save.setType(this.getClass().getName());
+        return save;
     }
 
     @Override
     public boolean submitted() {
-        return false;
+        return true;
     }
 }
