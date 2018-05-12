da57b6c47996465c52ba7d24f184da67b3eda3d8
eddy-hummel <eddy.hummel@epitech.eu>
2018-05-12 17:45:55 +0200

    [last_movie_by_genre] adding widget last movie by genre

---
+++ src/main/java/com/epitech/dashboard/Movies/MoviesWidgetLayout.java
@@ -0,0 +1,15 @@
+package com.epitech.dashboard.Movies;
+
+import com.vaadin.ui.Label;
+
+public class MoviesWidgetLayout extends MoviesListLayout {
+    public Label getTitle(){
+        return title;
+    }
+
+    public Label getDate(){
+        return date;
+    }
+
+    public Label getHead() {return head; }
+}
